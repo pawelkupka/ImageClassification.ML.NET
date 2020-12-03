39831c6a9b72a5c0b4eb6aa289b521db4b83d2d8
pawelkupka <pawel.kupka@gmail.com>
2020-12-03 22:17:01 +0100

    done

---
+++ DeepLearning_ImageClassification_Binary/ImageData.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+
+namespace DeepLearning_ImageClassification_Binary
+{
+    public class ImageData
+    {
+        [LoadColumn(0)]
+        public string ImagePath;
+
+        [LoadColumn(1)]
+        public string Label;
+    }
+}
---
+++ DeepLearning_ImageClassification_Binary/ImagePrediction.cs
@@ -0,0 +1,9 @@
+﻿namespace DeepLearning_ImageClassification_Binary
+{
+    public class ImagePrediction : ImageData
+    {
+        public float[] Score;
+
+        public string PredictedLabelValue;
+    }
+}
